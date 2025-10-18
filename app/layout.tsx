a0c26e9e3f3d9383a0924047b933efb40dc21ae7
bakxriyev <kamronbekbahriyev18@gmail.com>
2025-10-18 21:06:09 +0500

    dsds

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
+import Script from "next/script"
 import "./globals.css"
 
 const geist = Geist({ subsets: ["latin"] })
@@ -19,7 +20,37 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="uz">
-      <body className={`${geist.className} antialiased bg-[#1a1a1a]`}>{children}</body>
+      <head>
+        {/* ✅ Facebook Pixel scripti */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '647005224750459');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* ✅ Noscript versiyasi */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=647005224750459&ev=PageView&noscript=1"
+            alt="facebook pixel"
+          />
+        </noscript>
+      </head>
+      <body className={`${geist.className} antialiased bg-[#1a1a1a]`}>
+        {children}
+      </body>
     </html>
   )
 }
